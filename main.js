8fa8cf4d76589dac50e218940da3da75850e4496
hannes <33062605+HannesGitH@users.noreply.github.com>
2021-05-06 14:21:06 +0200

    removed old sample data

--- main.js
+++ main.js
@@ -1,20 +1,8 @@
 
-if (!true) {
-loadJSON('data-prep/replace.or.parse.our.own.json',
-(response)=>{
-    // Parse JSON string into object
-    //alternative parsing the .csv directly here would also work
-    var data = JSON.parse(response);
-    data = d3.hierarchy(data).sort((a, b) => d3.ascending(a.data.d_orig, b.data.d_orig))
-    console.log(data)
-
-    drawgraph(data)
-});
-}else{
 prepareData().then(data =>
     drawgraph(data)
 )
-}
+
 drawgraph = (data)=>{
     width = 954
     radius = width / 2
@@ -74,7 +62,7 @@ drawgraph = (data)=>{
             .attr("dy", "0.31em")
             .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
             .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
-            .text(d => /*{if (d.data[1] != undefined && d.data[1][0] != undefined) {return d.data[1][0].title} return*/ d.data.title || d.data.name /*}*/)
+            .text(d =>  d.data.title || d.data.name )
         .clone(true).lower()
             .attr("stroke", "white");
     
