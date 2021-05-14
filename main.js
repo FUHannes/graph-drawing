323de71fb0b678e2930ae4d7e960bd9f3a47dfd8
hannes <33062605+HannesGitH@users.noreply.github.com>
2021-05-14 16:10:27 +0200

    etwas aufgeräumt

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ prepareData().then(data =>
     drawgraph(data)
 )
 
+options = {
+    show_titles: true,
+    show_timescale: true,
+    form: 'circle',
+}
+
 drawgraph = (data)=>{
     width = 954
     radius = width / 2
@@ -25,82 +31,63 @@ drawgraph = (data)=>{
     
 //Zeitskala 1918-1980 remakes 1927-2018
 
-        function scale_radius(d){
-            console.log(d.y)
-            return 250 *(parseInt(d.data.year)-1918)/100 +200
+        function scale_year(year){
+            return 250 *(parseInt(year)-1918)/100 + (options.show_titles ? 200 : 50)
         }
+        function scale_radius(d){
+            return scale_year(d.data.year)
+        }
+        
+        const transform = d => `
+        rotate(${d.x * 180 / Math.PI - 90})
+        translate(${scale_radius(d)},0)
+        `
+        
 
         //links zwischen datenpunkten
         svg.append("g")
-            .attr("fill", "none")
-            .attr("stroke", "#555")
-            .attr("stroke-opacity", 0.4)
-            .attr("stroke-width", 1.5)
+            .attr("isLink", true)
         .selectAll("path")
         .data(root.links())
         .join("path")
             .attr("d", d3.linkRadial()
                 .angle(d => d.x)
-                .radius(d => scale_radius(d)))//*( parseInt(d.data.year)|| 1)));
+                .radius(d => scale_radius(d))
+            )
         
         // knotenpunkte selbst
-        svg.append("g")
+        console.log(svg.append("g")
         .selectAll("circle") //was macht das?
         .data(root.descendants())
         .join("circle")
-            .attr("transform", d => `
-            `//rotate(${d.x * 180 / Math.PI - 90})
-            +`
-            translate(${scale_radius(d)},0)
-            `)
-            .attr("opacity", d => d.children ? ".5" : "1")
+            .attr("transform", transform)
             .attr("fill", d => `hsl(${d.data.id/92*360},100%,50%)`)
-            .attr("r", 2.5);
+            .attr("opacity", d => d.children ? ".5" : "1")
+            .attr("isKnot", true))
     
         //beschriftung
         svg.append("g")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", 10)
-            .attr("stroke-linejoin", "round")
-            .attr("stroke-width", 3)
+            .attr("isTitles", true)
         .selectAll("text")
         .data(root.descendants())
         .join("text")
-            .attr("transform", d => `
-            rotate(${d.x * 180 / Math.PI - 90}) 
-            translate(${scale_radius(d)},0) 
+            .attr("transform", d => transform(d)+`
             rotate(${d.x >= Math.PI ? 180 : 0})
             `)
-            .on('mouseover', function (d, i) {
-                d3.select(this).transition()
-                     .duration('300')
-                     .attr('opacity', '1');
-           })
-           .on('mouseout', function (d, i) {
-            d3.select(this).transition()
-                 .duration('300')
-                 .attr('opacity', '.3');
-            })
             .attr("dy", "0.31em")
             .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
             .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
             .text(d =>  d.data.title )
-            
-            .attr('opacity', '0')
+            .attr('visibility', options.show_titles ? 'visible' : 'hidden')
             .clone(true).lower()
             .attr("stroke", "white");
     
         //jahrzenteringe
-        if (false){
-            for(jahrzent=0;jahrzent<11;jahrzent++){
-                //d.data.year = 1980
+        if (options.show_timescale){
+            for(jahrzent=2;jahrzent<11;jahrzent++){
                 svg.append('circle')
-                .attr('cx', 0)
-                .attr('cy', 0)
-                .attr('r', 238*(jahrzent*10))
-                .attr('stroke', '#111')
-                .attr('opacity', '.4')
-                .attr('fill', 'None');
+                .attr('r', scale_year(1900+jahrzent*10))
+                .attr('isRing', true)
             }
         }
         return svg.attr("viewBox", autoBox).node();
